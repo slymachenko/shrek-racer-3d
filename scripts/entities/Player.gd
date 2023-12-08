2edeeda3c6170d46506da1c82df349c312a6c1ea
SENYa-408 <senya.s408@gmail.com>
2023-12-08 17:48:12 +0100

    nitro added

--- scripts/entities/Player.gd
+++ scripts/entities/Player.gd
@@ -1,21 +1,25 @@
 extends VehicleBody3D
 
-const MAX_STEER = 0.8
-const ENGINE_POWER = 300
+const MAX_STEER_NORMAL = 0.8
+const MAX_STEER_NITRO = 0.1
+const ENGINE_POWER_NORMAL = 300
+const ENGINE_POWER_NITRO = 400
 
 @onready var camera_pivot = $CameraPivot
 @onready var front_camera = $CameraPivot/FrontCamera
 @onready var reverse_camera = $CameraPivot/ReverseCamera
 
 var look_at
+var max_steer = MAX_STEER_NORMAL
+var engine_power = ENGINE_POWER_NORMAL
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	look_at = global_position
 
 func _physics_process(delta):
-	steering = move_toward(steering, Input.get_axis("right", "left") * MAX_STEER, delta * 2.5)
-	engine_force = Input.get_axis("backward", "forward") * ENGINE_POWER
+	steering = move_toward(steering, Input.get_axis("right", "left") * max_steer, delta * 2.5)
+	engine_force = Input.get_axis("backward", "forward") * engine_power 
 	
 	camera_pivot.global_position = camera_pivot.global_position.lerp(global_position, delta * 20.0)
 	camera_pivot.transform = camera_pivot.transform.interpolate_with(transform, delta * 5.0)
@@ -23,6 +27,15 @@ func _physics_process(delta):
 	front_camera.look_at(look_at)
 	reverse_camera.look_at(look_at)
 	_check_camera_switch()
+
+func _input(event):
+	# NITRO
+	if event.is_action_pressed("nitro"):
+		max_steer = MAX_STEER_NITRO
+		engine_power = ENGINE_POWER_NITRO
+	if event.is_action_released("nitro"):
+		max_steer = MAX_STEER_NORMAL
+		engine_power = ENGINE_POWER_NORMAL
 
 func _check_camera_switch():
 	if linear_velocity.dot(transform.basis.z) >= 0:
--- scripts/world/Level.gd
+++ scripts/world/Level.gd
@@ -3,10 +3,14 @@ extends Node3D
 @onready var pause_menu = $PauseMenu
 @onready var player = $Player
 
+@onready var engine_force = $GUI/Info/engine_force
+@onready var max_steer = $GUI/Info/max_steer
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	engine_force.text = "engine_force: %s" % [player.engine_force]
+	max_steer.text = "max_steer: %s" % [player.max_steer]
