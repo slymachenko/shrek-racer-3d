b81b187b7b974448ad79cc3e2c585c43b9331d43
SENYa-408 <senya.s408@gmail.com>
2023-12-08 19:39:27 +0100

    player camera fixed

--- scripts/entities/Player.gd
+++ scripts/entities/Player.gd
@@ -6,16 +6,11 @@ const ENGINE_POWER_NORMAL = 300
 const ENGINE_POWER_NITRO = 400
 
 @onready var camera_pivot = $CameraPivot
-@onready var front_camera = $CameraPivot/FrontCamera
-@onready var reverse_camera = $CameraPivot/ReverseCamera
+@onready var front_camera = $CameraPivot/FrontSpringArm/FrontCamera
+@onready var reverse_camera = $CameraPivot/ReverseSpringArm/ReverseCamera
 
-var look_at
 var max_steer = MAX_STEER_NORMAL
 var engine_power = ENGINE_POWER_NORMAL
-
-func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	look_at = global_position
 
 func _physics_process(delta):
 	steering = move_toward(steering, Input.get_axis("right", "left") * max_steer, delta * 2.5)
@@ -23,12 +18,13 @@ func _physics_process(delta):
 	
 	camera_pivot.global_position = camera_pivot.global_position.lerp(global_position, delta * 20.0)
 	camera_pivot.transform = camera_pivot.transform.interpolate_with(transform, delta * 5.0)
-	look_at = look_at.lerp(global_position + linear_velocity, delta * 5.0)
-	front_camera.look_at(look_at)
-	reverse_camera.look_at(look_at)
-	_check_camera_switch()
 
 func _input(event):
+	# LOOK BACK
+	if event.is_action_pressed("look_back"):
+		reverse_camera.current = true
+	if event.is_action_released("look_back"):
+		front_camera.current = true
 	# NITRO
 	if event.is_action_pressed("nitro"):
 		max_steer = MAX_STEER_NITRO
@@ -36,9 +32,3 @@ func _input(event):
 	if event.is_action_released("nitro"):
 		max_steer = MAX_STEER_NORMAL
 		engine_power = ENGINE_POWER_NORMAL
-
-func _check_camera_switch():
-	if linear_velocity.dot(transform.basis.z) >= 0:
-		front_camera.current = true
-	else:
-		reverse_camera.current = true
--- scripts/world/Level.gd
+++ scripts/world/Level.gd
@@ -8,7 +8,7 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
